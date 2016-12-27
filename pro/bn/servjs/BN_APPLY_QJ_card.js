af31b96566e64014e0af96eff4df51de24834822
anyunfei <anyunfei549@126.com>
2016-12-27 21:03:53 +0800

    请假天数自动计算，修复常用下载更多的bug

--- pro/bn/servjs/BN_APPLY_QJ_card.js
+++ pro/bn/servjs/BN_APPLY_QJ_card.js
@@ -1,11 +1,32 @@
 var _viewer = this;
-_viewer.getItem("QJ_END_TIME").change(function(){
-	//if(_viewer.getItem("QJ_START_TIME") != ""){
-	if(_viewer.getItem("QJ_START_TIME").getValue().length>0){
-	
-		var dateDiff = rhDate.doDateDiff("D",_viewer.getItem("QJ_START_TIME").getValue(),_viewer.getItem("QJ_END_TIME").getValue(),0);
-	_viewer.getItem("QJ_NUMBER").setValue(dateDiff);
+var QJ_START_TIME = _viewer.getItem("QJ_START_TIME");
+var QJ_END_TIME = _viewer.getItem("QJ_END_TIME");
+var QJ_NUMBER = _viewer.getItem("QJ_NUMBER");
+jQuery(QJ_START_TIME.obj).addClass("Wdate").unbind("click").bind("click", function(){
+	WdatePicker({
+		dateFmt:"yyyy-MM-dd",
+		startDate:"%yyyy-%MM-%dd",
+		maxDate:QJ_END_TIME.getValue(),
+		onpicked:function(){
+			countDay();
 		}
-	
 	});
-	
+});
+
+jQuery(QJ_END_TIME.obj).addClass("Wdate").unbind("click").bind("click", function(){
+	WdatePicker({
+		dateFmt:"yyyy-MM-dd",
+		startDate:"%yyyy-%MM-%dd",
+		minDate:QJ_START_TIME.getValue(),
+		onpicked:function(){
+			countDay();
+		}
+	});
+});
+
+/**计算出差天数*/
+function countDay(){
+		if(QJ_START_TIME.getValue().length> 0 && QJ_END_TIME.getValue().length>0){
+		QJ_NUMBER.setValue(1+rhDate.doDateDiff("D", QJ_START_TIME.getValue(), QJ_END_TIME.getValue(), 0));
+		}
+}
--- pro/bn/servjs/BN_MH_DOWNLOAD_card.js
+++ pro/bn/servjs/BN_MH_DOWNLOAD_card.js
@@ -2,5 +2,5 @@ var _viewer = this;
 _viewer.getItem("FUJIAN").afterUploadCallback = function(fileData){
 	_viewer.getItem("LINK_NAME").setValue(fileData[0]["FILE_NAME"]);
 	/*_viewer.getItem("FILE_SIZE").setValue(fileData[0]["FILE_SIZE"]); */
-	_viewer.getItem("FILE_ID").setValue(fileData[0]["DATA_ID"]);
+	_viewer.getItem("FILE_ID").setValue(fileData[0]["FILE_ID"]);
 }
